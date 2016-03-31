da546a84ed8f070d585a8aa57acccfd169892853
tuncay <tuncay@srdc.com.tr>
2016-03-31 11:10:39 +0300

    Fix some errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
-if [ $SPARK_MODE = "master" ]
-then
-  $SPARK_HOME/sbin/start-master.sh 
-elif [ $SPARK_MODE = "slave" ]
-then
-  $SPARK_HOME/sbin/start-slave.sh spark://spark-master:7077
-else
-   echo "Unrecognized spark run mode"
+set -e
+
+if [[ -z "$SPARK_MODE" ]]; then
+    export SPARK_MODE="master"
 fi
 
-/cassandra-entrypoint.sh cassandra -f &
+if [ "$SPARK_MODE" = "master" ]; then
+  if [[ -z "$CASSANDRA_LISTEN_ADDRESS" ]]; then
+     export CASSANDRA_LISTEN_ADDRESS="auto"
+  fi     
+ 	
+  echo "Starting Spark master..."
 
-/bin/bash
+  $SPARK_HOME/sbin/start-master.sh && /cassandra-entrypoint.sh cassandra -f 
+
+  echo "Cassandra and Spark started ...."
+
+elif [ "$SPARK_MODE" = "slave" ]; then
+  if [[ -z "$SPARK_MASTER_URL" ]]; then
+     echo "Enviornment variable SPARK_MASTER_URL is missing !!!"
+     exit  	  
+  fi
+  
+  if [[ -z "$CASSANDRA_SEEDS" ]]; then
+     echo "Enviornment variable CASSANDRA_SEEDS is missing!!!"
+     exit
+  fi
+
+  echo "Starting Spark slave..."
+
+  $SPARK_HOME/sbin/start-slave.sh $SPARK_MASTER_URL && /cassandra-entrypoint.sh cassandra -f
+
+  echo "Cassandra and Spark started ...."
+else
+   echo "Unrecognized spark run mode, use either 'master' or 'slave'"
+fi
+
